3e3a792fa13ed0ec2144ea252f4d2a572d25fcf7
Cylush <jacobfarrow@hotmail.ca>
2019-11-08 21:53:50 -0500

    whoops

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = require("./config.json")
+//const token = require("./config.json")
 
 client.once('ready', () => {
     console.log('Ready!');
